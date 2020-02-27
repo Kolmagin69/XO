f8f7c27bece50cbbbadf2a01d2589b220f9421c4
Kolmgin69 <nikone.12.95@yandex.ru>
2020-02-27 16:36:59 +0300

    new change

--- scr/com/XO/Main.java
+++ scr/com/XO/Main.java
@@ -4,6 +4,6 @@ import com.XO.model.Figure;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Figure.X.toString() + "7");
+        System.out.println(Figure.X.toString() + "69");
     }
 }
