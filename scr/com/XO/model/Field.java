2fc0e069b206ddbafaccdaec29391892e5de5787
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-02-27 17:24:21 +0300

    #stage of development changes from 27.02.20

--- scr/com/XO/model/Field.java
+++ scr/com/XO/model/Field.java
@@ -1,5 +1,11 @@
 package com.XO.model;
 
 public class Field {
-    private int x = 0;
+    private int size;
+    private Figure[] figures;
+
+    public Field(int size) {
+        this.size = size;
+        figures = new Figure[size];
+    }
 }
