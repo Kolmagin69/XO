aef6b1b461deb516037bdce0977a03ccc45e7708
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-02 14:51:44 +0300

    An exception is thrown if you try to put "null" in a field

---
+++ scr/com/XO/model/exceptions/IncorrectFigureException.java
@@ -0,0 +1,7 @@
+package com.XO.model.exceptions;
+
+public class IncorrectFigureException extends RuntimeException {
+    public IncorrectFigureException (String messege) {
+        super(messege);
+    }
+}
