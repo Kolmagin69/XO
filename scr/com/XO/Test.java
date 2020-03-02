15b18be6dee6643c8c8321fa7ef23c3d5bc61593
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-02 14:32:20 +0300

    #stage of development 02/03/2020

--- scr/com/XO/Test.java
+++ scr/com/XO/Test.java
@@ -1,24 +1,42 @@
 package com.XO;
 
 import com.XO.model.*;
-import com.XO.view.Show;
+import com.XO.model.Point;
+import com.XO.view.*;
 
 import java.awt.*;
 
 public class Test {
     public static void main(String[] args) {
-        Point p1 = new Point(1, 1);
-        Point p2 = new Point(0, 0);
-        Point p3 = new Point(2, 1);
-        Point p4 = new Point(1, 2);
-        Point p5 = new Point(2, 2);
-        Field field = new Field(3);
-        field.setFigures(p1, Figure.X);
-        field.setFigures(p2, Figure.X);
-        field.setFigures(p3, Figure.O);
-        field.setFigures(p4, Figure.O);
-        field.setFigures(p5, Figure.X);
+
+        final Field field = new Field(3);
+        field.setFigures(Point.X0_Y0, Figure.X);
+        field.setFigures(Point.X0_Y1, Figure.O);
+        field.setFigures(Point.X0_Y2, Figure.X);
+        field.setFigures(Point.X1_Y0, Figure.O);
+        field.setFigures(Point.X1_Y1, Figure.X);
+        field.setFigures(Point.X1_Y2, Figure.O);
+        field.setFigures(Point.X2_Y0, Figure.X);
+        field.setFigures(Point.X2_Y1, Figure.O);
+        field.setFigures(Point.X2_Y2, Figure.X);
+
+        final Player playerX = new Player("Nikita", Figure.X);
+        final Player playerO = new Player("Kristina", Figure.O);
+        final GameForTwo gameXO = GameForTwo
+                .newBuider()
+                .setPlayerOne(playerX)
+                .setPlayerTwo(playerO)
+                .setField(field)
+                .build();
         Show.ShowField(field);
+
+
+
+
+
+
+
+
 
 
 
