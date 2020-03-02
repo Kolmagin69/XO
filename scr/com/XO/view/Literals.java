203d4518a8c172ab1b9986d1e92b28bb7cbbbb1a
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-02 17:23:09 +0300

    add Literals

---
+++ scr/com/XO/view/Literals.java
@@ -0,0 +1,8 @@
+package com.XO.view;
+
+public class Literals {
+    public static final char upLeft = '\u23BE';
+    public static final char downLeft = '\u23BF';
+    public static final char upMidel = '\u23C0';
+
+}
