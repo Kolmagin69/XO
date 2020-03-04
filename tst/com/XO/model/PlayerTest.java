bf8613f5dfcf7ff40631d12c9e265bb93a3eaf7a
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-04 14:13:33 +0300

    PlayerTest.java created

--- tst/com/XO/model/PlayerTest.java
+++ tst/com/XO/model/PlayerTest.java
@@ -1,5 +1,6 @@
 package com.XO.model;
 
+import com.XO.model.exceptions.IncorrectFigureException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +9,21 @@ class PlayerTest {
 
     @Test
     void getName() {
+        final String inputValue = "Black Man";
+        final String expectedValue = inputValue;
+        final Player player = new Player(inputValue, Figure.X);
+        final String actualValue = player.getName();
+        assertEquals(expectedValue, actualValue);
+
     }
 
     @Test
     void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+        final Player player = new Player("Name", inputValue);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue, actualValue);
+
     }
 }
