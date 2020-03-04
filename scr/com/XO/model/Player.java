809305476c2caf3f4537e86a1887828dccde180e
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-04 14:19:57 +0300

    add exceptions

--- scr/com/XO/model/Player.java
+++ scr/com/XO/model/Player.java
@@ -1,10 +1,16 @@
 package com.XO.model;
 
+import com.XO.model.exceptions.IncorrectFigureException;
+
 public class Player {
     private final String name;
     private final Figure figure;
 
-    public Player(final String name, final Figure figure) {
+    public Player(final String name, final Figure figure) throws IncorrectFigureException {
+        if(figure == null) {
+            throw new IncorrectFigureException
+                    ("\n\"You can\'t put \"null\" in to the field!\"\n\"Try \"Figure.X\" or \"Figure.O\"\"");
+        }
         this.name = name;
         this.figure = figure;
     }
