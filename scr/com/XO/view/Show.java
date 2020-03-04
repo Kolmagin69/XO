389d3e87bb57b4d25cad1d9dacd0d9a2aaacc005
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-04 12:33:54 +0300

    add new lines

--- scr/com/XO/view/Show.java
+++ scr/com/XO/view/Show.java
@@ -5,19 +5,21 @@ import com.XO.model.*;
 
 public class Show {
 
-    private static final String crossLine = "-------------\n";
-
     public static void ShowField(final Field field) {
-        StringBuilder strLocal1 = new StringBuilder(crossLine);
+        StringBuilder strLocal1 = new StringBuilder(Lines.TOP_LINE);
         for (int j = 0; j < field.getSize() ; j++) {
-            strLocal1.append(horizontalLine(j, field) + crossLine);
+            if(j < field.getSize() - 1) {
+                strLocal1.append(horizontalLine(j, field) + Lines.MIDEL_LINE);
+            } else {
+                strLocal1.append(horizontalLine(j, field) + Lines.BOTTOM_LINE);
+            }
         }
         System.out.println(strLocal1);
     }
 
     private static String horizontalLine(final int a, final Field field) {
         Figure[][] figureLocal = field.getFigures();
-        StringBuilder strLocal = new StringBuilder("|");
+        StringBuilder strLocal = new StringBuilder();
         for (int i = 0; i < field.getSize(); i++) {
             Figure figureInMetod = figureLocal[a][i];
             if(figureInMetod == null) {
@@ -26,9 +28,47 @@ public class Show {
                 strLocal.append(appendSpace(figureInMetod.toString()));
             }
         }
-        return strLocal.toString() + "\n";
+        return Lines.DASH_VERTICAL + strLocal.toString() + "\n";
     }
     private  static String appendSpace(final String str) {
-        return " " + str + " |";
+        return "  " + str + "  " + Lines.DASH_VERTICAL;
     }
-}
+
+    private static class Lines{
+        private static final char DASH_HORIZONTAL = '\u2501';
+        private static final char DASH_VERTICAL = '\u2503';
+
+        private static final char UP_LEFT = '\u250F';
+        private static final char UP_RIGHT = '\u2513';
+        private static final char UP_MIDEL = '\u2533';
+
+        private static final char MIDEL_LEFT = '\u2523';
+        private static final char MIDEL_MIDEL = '\u254B';
+        private static final char MIDEL_RIGHT = '\u252B';
+
+        private static final char DOWN_LEFT = '\u2517';
+        private static final char DOWN_MIDEL = '\u253B';
+        private static final char DOWN_RIGHT = '\u251B';
+
+
+        private static final String TOP_LINE = new String(new char[]{
+                UP_LEFT, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                UP_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                UP_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                UP_RIGHT, '\n'});
+
+        private static final String MIDEL_LINE = new String(new char[]{
+                MIDEL_LEFT, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                MIDEL_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                MIDEL_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                MIDEL_RIGHT, '\n'});
+
+        private static final String BOTTOM_LINE = new String(new char[]{
+                DOWN_LEFT, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                DOWN_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                DOWN_MIDEL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL, DASH_HORIZONTAL,
+                DOWN_RIGHT, '\n'});
+        }
+
+    }
+
