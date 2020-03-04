35bae4c3dd66252f6c0c25b449130a828d623347
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-04 14:19:06 +0300

    PlayerTest.java created

--- scr/com/XO/Test.java
+++ scr/com/XO/Test.java
@@ -8,11 +8,12 @@ import java.awt.*;
 
 public class Test {
     public static void main(String[] args) {
+        //System.out.println(Line.arrayChar<1>.toString());
 
         final Field field = new Field(3);
         field.setFigures(Point.X0_Y0, Figure.X);
         field.setFigures(Point.X0_Y1, Figure.O);
-        field.setFigures(Point.X0_Y2, null);
+        field.setFigures(Point.X0_Y2, Figure.X);
         field.setFigures(Point.X1_Y0, Figure.O);
         field.setFigures(Point.X1_Y1, Figure.X);
         field.setFigures(Point.X1_Y2, Figure.O);
@@ -20,15 +21,23 @@ public class Test {
         field.setFigures(Point.X2_Y1, Figure.O);
         field.setFigures(Point.X2_Y2, Figure.X);
 
-        final Player playerX = new Player("Nikita", Figure.X);
-        final Player playerO = new Player("Kristina", Figure.O);
-        final GameForTwo gameXO = GameForTwo
-                .newBuider()
-                .setPlayerOne(playerX)
-                .setPlayerTwo(playerO)
-                .setField(field)
+//
+//        final Player playerX = new Player("Nikita", Figure.X);
+//        final Player playerO = new Player("Kristina", Figure.O);
+//        final GameForTwo gameXO = GameForTwo
+//                .newBuider()
+//                .setPlayerOne(playerX)
+//                .setPlayerTwo(playerO)
+//                .setField(field)
+//                .build();
+//        Show.ShowField(field);
+        Game myGame = new Game.Builder()
+                .field(field)
                 .build();
-        Show.ShowField(field);
+
+       Show.ShowField((field));
+
+
     }
 }
 
