74bebb2d3b9a20fa4344dcbf09c25c0d4d7da62f
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-05 13:55:21 +0300

    add checking the name for exceptions

--- scr/com/XO/model/Player.java
+++ scr/com/XO/model/Player.java
@@ -1,19 +1,26 @@
 package com.XO.model;
 
-import com.XO.model.exceptions.IncorrectFigureException;
+import com.XO.model.exceptions.*;
 
 public class Player {
     private final String name;
     private final Figure figure;
 
-    public Player(final String name, final Figure figure) throws IncorrectFigureException {
+    public Player(final String name, final Figure figure) throws IncorrectFigureException, IncorrectNameException {
         if(figure == null) {
             throw new IncorrectFigureException
-                    ("\n\"You can\'t put \"null\" in to the field!\"\n\"Try \"Figure.X\" or \"Figure.O\"\"");
+                    ("\nYou can\'t put \"null\" in to the field!\nTry \"Figure.X\" or \"Figure.O\"");
+        }
+        if(!checkName(name)) {
+            throw  new IncorrectNameException
+                    ("\nIncorrect player name. Wrong player name options:\n1 - \"\"\n2 - \"  \"\n3 - null\n" +
+                            "The name must contain at least one character! Example of the correct name:\n1 - \"Nikita\"\n2 - \" nIKiTA\"\n3 - \"N\"");
         }
         this.name = name;
         this.figure = figure;
     }
+
+
 
     public String getName() {
         return name;
@@ -22,4 +29,9 @@ public class Player {
     public Figure getFigure() {
         return figure;
     }
+    public static boolean checkName(String name) {
+        if(name == null || name.isEmpty() || name.trim().isEmpty()) return false;
+        return true;
+    }
+
 }
