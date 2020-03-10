686ec128cdbbaab1fb73473f43eed958a8a9ea92
Kolmgin69 <nikone.12.95@yandex.ru>
2020-03-10 23:25:35 +0300

    created class MoveController

--- scr/com/XO/controller/MoveController.java
+++ scr/com/XO/controller/MoveController.java
@@ -1,5 +1,18 @@
 package com.XO.controller;
 
+import com.XO.model.*;
+import com.XO.model.exceptions.IncorrectCoordinatException;
+
 public class MoveController {
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws IncorrectCoordinatException {
+
+        if(field.getFigure(point) != null)
+            throw new IncorrectCoordinatException
+                    ("\n\"The field is not free!\"\n\"Try another point.\"");
+
+    }
+
 
 }
