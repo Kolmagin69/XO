0af3fe95697d6d6bb7014fce743945cbc903e86d
Kolmgin69 <nikone.12.95@yandex.ru>
2020-03-11 23:14:50 +0300

    add method checkLineWinner()

--- scr/com/XO/controller/WinnerController.java
+++ scr/com/XO/controller/WinnerController.java
@@ -2,9 +2,38 @@ package com.XO.controller;
 
 import com.XO.model.Field;
 import com.XO.model.Figure;
+import com.XO.model.Point;
 
 public class WinnerController {
     public static Figure getWinner(final Field field){
+        return  null;
+    }
+    public static Figure checkLineWinner(final Field field) {
+        for (int i = 0; i < field.getSize(); i++) {
+            int counterX = 0;
+            int counterO = 0;
+            for (int j = 0; j < field.getSize(); j++) {
+                if (field.getFigure(Point.getPoint(i,j)) == Figure.X) counterX ++;
+                if (field.getFigure(Point.getPoint(i,j)) == Figure.O) counterO ++;
+                if (counterX == field.getSize()) return  Figure.X;
+                if (counterO == field.getSize()) return Figure.O;
+            }
+            counterO = 0;
+            counterX = 0;
+        }
+        for (int i = 0; i < field.getSize(); i++) {
+            int counterX = 0;
+            int counterO = 0;
+            for (int j = 0; j < field.getSize(); j++) {
+                if (field.getFigure(Point.getPoint(j,i)) == Figure.X) counterX ++;
+                if (field.getFigure(Point.getPoint(j,i)) == Figure.O) counterO ++;
+                if (counterX == field.getSize()) return  Figure.X;
+                if (counterO == field.getSize()) return Figure.O;
+            }
+            counterO = 0;
+            counterX = 0;
+        }
+
         return  null;
     }
 }
