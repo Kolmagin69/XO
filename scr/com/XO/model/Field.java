17db6df54f779f69d9bd35e6c08ffccd72d9e37f
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-12 13:18:03 +0300

    add class field counter, which counts the number of shapes on the field

--- scr/com/XO/model/Field.java
+++ scr/com/XO/model/Field.java
@@ -5,6 +5,7 @@ import com.XO.model.exceptions.*;
 
 public class Field {
     private final int size;
+    private int counterFigure = 0;
     private final Figure[][] figures;
 
 
@@ -25,6 +26,7 @@ public class Field {
             throw new IncorrectFigureException
                     ("\n\"You can\'t put \"null\" in to the field!\"\n\"Try \"Figure.X\" or \"Figure.O\"\"");
         figures[point.getX()][point.getY()] = figure;
+        counterFigure++;
     }
 
     public Figure getFigure(final Point point) {
@@ -34,6 +36,10 @@ public class Field {
 
     public Figure[][] getFigures() {
         return figures;
+    }
+
+    public int getCounterFigure(){
+        return counterFigure;
     }
 
 
