345f4169a4f2913e0954ec56604a77fa54f96359
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-12 13:18:30 +0300

    add method testGetFigure()

--- tst/com/XO/model/FieldTest.java
+++ tst/com/XO/model/FieldTest.java
@@ -1,5 +1,6 @@
 package com.XO.model;
 
+import com.XO.controller.MoveController;
 import com.XO.model.exceptions.IncorrectCoordinatException;
 import com.XO.model.exceptions.IncorrectFigureException;
 import org.junit.jupiter.api.Test;
@@ -83,5 +84,33 @@ class FieldTest {
        field.setFigures(Point.X1_Y1, Figure.X);
        final Figure[][] actualFigures = field.getFigures();
        assertEquals(expectedFigures[1][1], actualFigures[1][1]);
+    }
+
+    @Test
+    void getCounterFigure() {
+        final Field field = new Field(3);
+        field.setFigures(Point.X0_Y0, MoveController.currentFigure(field));
+        final int actualNumberOfFigure1 = field.getCounterFigure();
+
+        assertEquals(1, actualNumberOfFigure1);
+
+        field.setFigures(Point.X1_Y0, MoveController.currentFigure(field));
+        field.setFigures(Point.X2_Y0, MoveController.currentFigure(field));
+        final int actualNumberOfFigure2 = field.getCounterFigure();
+
+        assertEquals(3, actualNumberOfFigure2);
+
+        field.setFigures(Point.X0_Y1, MoveController.currentFigure(field));
+        field.setFigures(Point.X0_Y2, MoveController.currentFigure(field));
+        field.setFigures(Point.X1_Y1, MoveController.currentFigure(field));
+        field.setFigures(Point.X1_Y2, MoveController.currentFigure(field));
+        field.setFigures(Point.X2_Y1, MoveController.currentFigure(field));
+        field.setFigures(Point.X2_Y2, MoveController.currentFigure(field));
+        final int actualNumberOfFigure3 = field.getCounterFigure();
+
+        assertEquals(9, actualNumberOfFigure3);
+
+
+
     }
 }
