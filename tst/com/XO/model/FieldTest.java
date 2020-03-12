4c7bf95a448aeb7f155de7dc065bf12d5c8c8f78
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-12 14:10:53 +0300

    renamed method testGetCounterFigure()

--- tst/com/XO/model/FieldTest.java
+++ tst/com/XO/model/FieldTest.java
@@ -87,7 +87,7 @@ class FieldTest {
     }
 
     @Test
-    void getCounterFigure() {
+    void testGetCounterFigure() {
         final Field field = new Field(3);
         field.setFigures(Point.X0_Y0, MoveController.currentFigure(field));
         final int actualNumberOfFigure1 = field.getCounterFigure();
