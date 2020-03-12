519d05a042fae5fcd4a2ffe4550faf96dea68edf
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-12 17:23:05 +0300

    change

--- scr/com/XO/controller/WinnerController.java
+++ scr/com/XO/controller/WinnerController.java
@@ -6,13 +6,14 @@ import com.XO.model.Point;
 
 public class WinnerController {
     public static Figure getWinner(final Field field){
-
+        if (checkLineWinner(field) != null) return checkLineWinner(field);
+        if (fieldIsFull(field)) return null;
         return  null;
     }
     public static Figure checkLineWinner(final Field field) {
+        int counterX = 0;
+        int counterO = 0;
         for (int i = 0; i < field.getSize(); i++) {
-            int counterX = 0;
-            int counterO = 0;
             for (int j = 0; j < field.getSize(); j++) {
                 if (field.getFigure(Point.getPoint(i,j)) == Figure.X) counterX ++;
                 if (field.getFigure(Point.getPoint(i,j)) == Figure.O) counterO ++;
@@ -23,8 +24,6 @@ public class WinnerController {
             counterX = 0;
         }
         for (int i = 0; i < field.getSize(); i++) {
-            int counterX = 0;
-            int counterO = 0;
             for (int j = 0; j < field.getSize(); j++) {
                 if (field.getFigure(Point.getPoint(j,i)) == Figure.X) counterX ++;
                 if (field.getFigure(Point.getPoint(j,i)) == Figure.O) counterO ++;
@@ -34,17 +33,30 @@ public class WinnerController {
             counterO = 0;
             counterX = 0;
         }
-
+        for (int i = 0; i < field.getSize(); i++) {
+            if (field.getFigure(Point.getPoint(i,i)) == Figure.X) counterX++;
+            if (field.getFigure(Point.getPoint(i,i)) == Figure.O) counterO++;
+            if (counterX == field.getSize()) return  Figure.X;
+            if (counterO == field.getSize()) return Figure.O;
+        }
+        counterO = 0;
+        counterX = 0;
+        int pointX = field.getSize();
+        for (int i = 0; i < field.getSize(); i++) {
+            pointX--;
+            if (field.getFigure(Point.getPoint(pointX,i)) == Figure.X) counterX++;
+            if (field.getFigure(Point.getPoint(pointX,i)) == Figure.O) counterO++;
+            if (counterX == field.getSize()) return  Figure.X;
+            if (counterO == field.getSize()) return Figure.O;
+        }
+        counterO = 0;
+        counterX = 0;
         return  null;
     }
-    public boolean fieldIsFull(final  Field field) {
-        int counter = 0;
-        for (int i = 0; i < field.getSize(); i++) {
-            for (int j = 0; j < field.getSize(); j++) {
-                if (field.getFigure(Point.getPoint(i,j)) != null) counter++;
-            }
-        }
-        if (counter == field.getSize() * field.getSize()) return true;
+    public static boolean fieldIsFull(final Field field) {
+        if (field.getCounterFigure() == field.getSize() * field.getSize()) return true;
         return false;
     }
+
+
 }
