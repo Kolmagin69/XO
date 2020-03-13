fccbdbf69b010fe20780a10b0a085fe236e5cc8c
Kolmgin69 <nikone.12.95@yandex.ru>
2020-03-14 00:41:38 +0300

    update class

--- scr/com/XO/controller/PrematureEndingController.java
+++ scr/com/XO/controller/PrematureEndingController.java
@@ -2,6 +2,7 @@ package com.XO.controller;
 
 import com.XO.model.Field;
 import com.XO.model.Figure;
+import com.XO.model.Game;
 import com.XO.model.GameForTwo;
 import com.XO.model.Point;
 
@@ -9,18 +10,25 @@ public class PrematureEndingController {
 
 
     public static boolean noMoveToWin(final GameForTwo game) {
-        Field field1 = game.getField();
-        final GameForTwo game2 = game;
-        Field field2 = game2.getField();
-        if (!fieldIsFull(field1)) {
-            setFigureOnFreePoint(Figure.X, game);
-        }
+        GameForTwo gameX = game;
+        GameForTwo gameO = game;
+        if (ItsFigureCanWin(Figure.X, gameX) || ItsFigureCanWin(Figure.O, gameO)) return false;
+        return true;
     }
+    private static boolean ItsFigureCanWin(final Figure figure, final GameForTwo game) {
+        final Field field = game.getField();
+        if (!fieldIsFull(field)) {
+            setFigureOnFreePoint(figure, game);
+        }
+        if (WinnerController.getWinner(field) != null) return true;
+        return  false;
+    }
+
     public static boolean fieldIsFull(final Field field) {
         if (field.getCounterFigure() == field.getSize() * field.getSize()) return true;
         return false;
     }
-    private static void setFigureOnFreePoint(final Figure figure, final GameForTwo game) {
+    private static void setFigureOnFreePoint(Figure figure, final GameForTwo game) {
         Field field = game.getField();
         final int size = field.getSize();
         for (int i = 0; i < size; i++) {
