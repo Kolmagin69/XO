b0ff9165ef2e55115daed360950debb8849aa10a
Kolmagin69 <55854802+Kolmagin69@users.noreply.github.com>
2020-03-13 17:29:39 +0300

    created new class

--- scr/com/XO/controller/PrematureEndingController.java
+++ scr/com/XO/controller/PrematureEndingController.java
@@ -1,22 +1,34 @@
 package com.XO.controller;
 
 import com.XO.model.Field;
+import com.XO.model.Figure;
 import com.XO.model.GameForTwo;
+import com.XO.model.Point;
 
 public class PrematureEndingController {
-    public static boolean noMoveToWin(GameForTwo game) {
-        Field field = game.getField();
-        final int size = field.getSize();
-        if (field.getCounterFigure() != size * size) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (field.getFigure())
-                }
-            }
+
+
+    public static boolean noMoveToWin(final GameForTwo game) {
+        Field field1 = game.getField();
+        final GameForTwo game2 = game;
+        Field field2 = game2.getField();
+        if (!fieldIsFull(field1)) {
+            setFigureOnFreePoint(Figure.X, game);
         }
     }
     public static boolean fieldIsFull(final Field field) {
         if (field.getCounterFigure() == field.getSize() * field.getSize()) return true;
         return false;
+    }
+    private static void setFigureOnFreePoint(final Figure figure, final GameForTwo game) {
+        Field field = game.getField();
+        final int size = field.getSize();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Point point = Point.getPoint(i, j);
+                if (field.getFigure(point) == null) field.setFigures(point, figure);
+            }
+        }
+
     }
 }
